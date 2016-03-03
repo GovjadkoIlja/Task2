46c235495ab2d0300e4de59fa540627779f643d0
Ольга Семакина <semakina.olga@gmail.com>
2016-03-03 23:36:38 +0300

    .

---
+++ Task2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    class Program
+    {
+        public static int find(int n)
+        {
+            int i=1;
+            int s = 0;
+            while (s + i < n)
+            {
+                s += i;
+                i++;
+            }
+            return (s == n - 1 ? 1 : 0);
+        }
+        static void Main(string[] args)
+        {
+            int i;
+            int j;
+            int n;
+            int m;
+            n=Convert.ToInt32(Console.ReadLine());
+            int[] mass = new int[n];
+            for (i = 0; i < n; i++)
+                mass[i] = Convert.ToInt32(Console.ReadLine());
+            for (i = 0; i < n; i++)
+            {
+                m = find(mass[i]);
+                Console.Write(m + " ");
+            }
+            Console.ReadKey();
+        }
+    }
+}
